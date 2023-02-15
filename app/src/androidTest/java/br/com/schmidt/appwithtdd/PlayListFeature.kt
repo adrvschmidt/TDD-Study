507fe24d1879c03059f522a7f7742fd1685eb2be
Adriano Schmidt <adrvschmidt@gmail.com>
2023-02-15 19:06:28 -0300

    Added first instrumentation test and changed the mainactivity label to Playlists

--- app/src/androidTest/java/br/com/schmidt/appwithtdd/PlayListFeature.kt
+++ app/src/androidTest/java/br/com/schmidt/appwithtdd/PlayListFeature.kt
@@ -1,12 +1,15 @@
 package br.com.schmidt.appwithtdd
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +17,13 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class PlayListFeature {
+
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    @Rule get
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("br.com.schmidt.appwithtdd", appContext.packageName)
+    fun displayScreenTitle() {
+        assertDisplayed("Playlists")
     }
 }
